df3feeb746890a58fa51dd341387c8c67b57512a
Ag SouL <47560765+AgSouL@users.noreply.github.com>
2019-03-15 11:38:49 +0000

    Version 2.0
    Добавлена возможность просмотра пользователем заявок;
    + подсчет стоимости заявки.

--- WindowsFormsApp2/OrdersList.Designer.cs
+++ WindowsFormsApp2/OrdersList.Designer.cs
@@ -37,6 +37,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rePassToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -44,9 +48,6 @@
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.rePassToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
@@ -55,6 +56,7 @@
             // groupBox1
             // 
             this.groupBox1.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.background_1939125_960_720;
+            this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
@@ -64,14 +66,14 @@
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 181);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(288, 154);
+            this.groupBox1.Size = new System.Drawing.Size(290, 154);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Add Delivery";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 123);
+            this.button1.Location = new System.Drawing.Point(122, 122);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 25);
             this.button1.TabIndex = 6;
@@ -81,7 +83,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(68, 105);
+            this.label4.Location = new System.Drawing.Point(180, 105);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 5;
@@ -90,7 +92,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 106);
+            this.label3.Location = new System.Drawing.Point(119, 106);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 4;
@@ -112,6 +114,7 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(161, 21);
             this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -129,6 +132,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(161, 21);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // dataGridView1
             // 
@@ -143,43 +147,8 @@
             this.Column7});
             this.dataGridView1.Location = new System.Drawing.Point(12, 32);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(640, 130);
+            this.dataGridView1.Size = new System.Drawing.Size(743, 130);
             this.dataGridView1.TabIndex = 1;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Number";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Date";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Type Clothing";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Volume";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Delivery";
-            this.Column5.Name = "Column5";
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Cost of";
-            this.Column6.Name = "Column6";
-            // 
-            // Column7
-            // 
-            this.Column7.HeaderText = "Acept";
-            this.Column7.Name = "Column7";
             // 
             // menuStrip1
             // 
@@ -188,7 +157,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(674, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(766, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -204,18 +173,72 @@
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.helpToolStripMenuItem.Text = "Help";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 106);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Delivery 35$";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "id";
+            this.Column1.HeaderText = "Number";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "date";
+            this.Column2.HeaderText = "Date";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "type";
+            this.Column3.HeaderText = "Type Clothing";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "counter";
+            this.Column4.HeaderText = "Volume";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "delivery";
+            this.Column5.HeaderText = "Delivery";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "total";
+            this.Column6.HeaderText = "Cost of";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "confirmed";
+            this.Column7.HeaderText = "Acept";
+            this.Column7.Name = "Column7";
+            // 
             // OrdersList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.background_1939125_960_720;
-            this.ClientSize = new System.Drawing.Size(674, 347);
+            this.ClientSize = new System.Drawing.Size(766, 347);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "OrdersList";
             this.Text = "OrdersList";
+            this.Load += new System.EventHandler(this.OrdersList_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -237,6 +260,10 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rePassToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
@@ -244,8 +271,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem rePassToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
     }
 }
--- WindowsFormsApp2/OrdersList.cs
+++ WindowsFormsApp2/OrdersList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,105 @@ namespace WindowsFormsApp2
 {
     public partial class OrdersList : Form
     {
-        private int id;
 
-        public OrdersList(AuthForm authForm)
+        private class Prices
         {
-            InitializeComponent();
+            public int id { get; set; }
+            public string name { get; set; }
+            public double price { get; set; }
+
+            public Prices(int i, string n, double p)
+            {
+                this.id = i;
+                this.name = n;
+                this.price = p;
+            }
+
         }
 
-        public OrdersList(AuthForm authForm, int id) : this(authForm)
+        int id;
+        AuthForm authform;
+        int userid;
+        SqlConnection connection = new SqlConnection(Properties.Settings.Default.dbConnectionString);
+        double total = 0;
+
+        public OrdersList(AuthForm a, int cid)
         {
-            this.id = id;
+            InitializeComponent();
+            this.authform = a;
+            this.userid = cid;
+        }
+
+        private void RefreshGrid()
+        {
+            string sql = "SELECT id, date, type, counter, delivery, total, confirmed FROM orders WHERE userid=" + this.userid;
+            connection.Open();
+            SqlCommand sqlcommand = new SqlCommand(sql, connection);
+            SqlDataAdapter sqladapter = new SqlDataAdapter(sqlcommand);
+            SqlCommandBuilder sqlbuilder = new SqlCommandBuilder(sqladapter);
+            DataSet ds = new DataSet();
+            sqladapter.Fill(ds, "orders");
+            DataTable dt = ds.Tables["orders"];
+            dataGridView1.DataSource = ds.Tables["orders"];
+        }
+
+
+        private void fillists()
+        {
+            string sql = "SELECT id, name, price FROM types";
+            SqlCommand sqlcommand = new SqlCommand(sql, connection);
+            SqlDataReader sqlreader = sqlcommand.ExecuteReader();
+            List<Prices> Listofprices = new List<Prices>();
+            while (sqlreader.Read())
+            {
+                Listofprices.Add(new Prices(Convert.ToInt32(sqlreader["id"]),
+                    sqlreader["name"].ToString(), Convert.ToDouble(sqlreader["price"])));
+            }
+            comboBox1.DataSource = Listofprices;
+            comboBox1.DisplayMember = "name";
+            sqlreader.Close();
+            System.Object[] ItemObject = new System.Object[10];
+            for (int i = 0; i <= 9; i++)
+            {
+                ItemObject[i] = i;
+            }
+            comboBox2.Items.AddRange(ItemObject);
+
+        }
+
+        private void calculate()
+        {
+            Prices current = comboBox1.SelectedItem as Prices;
+            int cnt = comboBox2.SelectedIndex;
+            if (cnt == -1) cnt =1;
+            total = current.price* cnt;
+            if(checkBox1.Checked)
+            {
+                total = total + 35;
+            }
+            label4.Text = total + "$";
+        }
+
+
+        private void OrdersList_Load(object sender, EventArgs e)
+        {
+            RefreshGrid();
+            fillists();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            calculate();
         }
     }
 }
